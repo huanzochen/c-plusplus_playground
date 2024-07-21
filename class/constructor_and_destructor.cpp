3eb15cdf95e1ec14c992b87e9cd3b37b7a6e24b9
tzuchuan.huang <cy94295@gmail.com>
2024-07-21 19:44:01 +0800

    feat: construtor and destructor

---
+++ class/constructor_and_destructor.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player
+{
+private:
+  string name;
+  int health;
+  int xp;
+
+public:
+  void set_name(string name_val)
+  {
+    name = name_val;
+  }
+
+  void yell()
+  {
+    cout << "HAHAHA!!" << endl;
+  }
+
+  void cry()
+  {
+    cout << "嗚嗚!!" << endl;
+  }
+
+  // Overload Constructors
+  Player()
+  {
+    cout << "No args constructor called" << endl;
+  }
+  Player(string name_val) : name{name_val}
+  {
+    cout << "String arg construtor called" << endl;
+  }
+  Player(string name_val, int health, int xp) : name{name_val}
+  {
+    cout << "3 args constructor called" << endl;
+  }
+  ~Player()
+  {
+    cout << "Destructor called for " << name << endl;
+
+    // maybe execute delete xxx if there is pointer has been newed.
+  }
+};
+
+int main()
+{
+  {
+    Player tommy;
+    tommy.set_name("tommy");
+  }
+
+  {
+    Player frank;
+    frank.set_name("Frank");
+    Player hero;
+    hero.set_name("hero");
+    Player villian("Villian", 100, 12);
+    villian.set_name("villian");
+  }
+
+  Player *enemy{new Player("enemy", 1000, 10)};
+  enemy->yell();
+
+  delete enemy;
+
+  Player *vivian{new Player("vivian", 10, 10)};
+  vivian->cry();
+
+  delete vivian;
+}
