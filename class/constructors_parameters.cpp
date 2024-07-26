677920085daf97a85e5988e2c3b860e94c70daaf
tzuchuan.huang <cy94295@gmail.com>
2024-07-26 17:12:27 +0800

    feat: construtor parameters

--- class/constructors_parameters.cpp
+++ class/constructors_parameters.cpp
@@ -26,6 +26,11 @@ public:
     cout << "嗚嗚!!" << endl;
   }
 
+  string crying()
+  {
+    return "嗚嗚！！";
+  }
+
   Player(string name_val = "None", int health = 0, int xp = 0);
 
   ~Player();
@@ -49,4 +54,9 @@ int main()
   Player hero{"Hero", 10};
   Player villian("Villian", 100, 12);
   villian.set_name("villian");
+
+  // It is able to call directly.
+  villian.cry();
+
+  cout << villian.crying() << endl;
 }
