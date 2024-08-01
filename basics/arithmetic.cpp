7176b504ae7288b849988aa483978c1a4def36a0
Huang, TzuChuan | Tommy | TWR <tzuchuan.huang@rakuten.com>
2024-08-01 17:58:35 +0800

    test: arithmetic

--- basics/arithmetic.cpp
+++ basics/arithmetic.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
+int main()
+{
+  cout << "8/10: " << 8 / 10 << endl;
+  cout << "13/10: " << 13 / 10 << endl;
 
-int main() {
-  cout << "8/10: " << 8/10 << endl;
-  cout << "13/10: " << 13/10 << endl;
+  cout << "0/2: " << 0 / 2 << endl;
+  cout << "1/2: " << 1 / 2 << endl;
 }
