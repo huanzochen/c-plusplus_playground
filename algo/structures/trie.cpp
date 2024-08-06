1f68e81d075baa713cfe8ad4da76df6f516869ab
tzuchuan.huang <cy94295@gmail.com>
2024-08-06 14:26:42 +0800

    feat: trie(undone)

--- algo/structures/trie.cpp
+++ algo/structures/trie.cpp
@@ -12,20 +12,22 @@ private:
   // Number of key value pairs in the map
   int size = 0;
 
-
   // 這是一個被動的資料結構，所以用 struct.
   struct TrieNode
   {
     V val;
-    array<TrieNode*, 256> children;
+    array<TrieNode *, 256> children;
 
     // Constructor
-    TrieNode(): val(V()){
+    TrieNode() : val(V())
+    {
       children.fill(nullptr);
     }
 
-    ~TrieNode(){
-      for(TrieNode* child: children){
+    ~TrieNode()
+    {
+      for (TrieNode *child : children)
+      {
         delete child;
       }
     }
@@ -64,7 +66,11 @@ public:
   // 判斷是和否存在前綴為 prefix 的鍵
   // hasKeyWithPrefix("tha") -> true
   // hasKeyWithPrefix("apple") -> true
-  bool hasKeyWithPrefix(string prefix);
+  bool hasKeyWithPrefix(string prefix)
+  {
+    // 只要找到一個 node，就是存在 prefix.
+    return getNode(root, prefix);
+  }
 
   // 通配符 . 匹配任意字符，搜索所有匹配的鍵
   // keysWithPattern(".ip") -> ["rip", "sip", "gip"]
@@ -77,7 +83,46 @@ public:
   bool hasKeyWithPattern(string pattern);
 
   // 返回 Map 鍵值對數量
-  int getSize() const {
+  int getSize() const
+  {
     return size;
+  }
+
+  V get(string key)
+  {
+    // 從 root 開始搜索 key
+    TrieNode<V> x = getNode(root, key);
+    if (x == null || x.val == null)
+    {
+      // x 為空或 x 的 val 字段都為空說明 key 沒有對應值
+      return null;
+    }
+    return x.val;
+  }
+
+  // Get Trie 對應的 Node, 搜索到了返回 Node.
+  // 對以下這句話有疑惑，先繼續看下去
+  // 這里需要注意，就算 getNode(key) 的返回值 x 非空，也只能说字符串 key 是一个「前缀」；除非 x.val 同时非空，才能判断键 key 存在。
+  TrieNode<V> *getNode(TrieNode<V> *node, const string &key)
+  {
+    TrieNode<V> *p = node;
+
+    // 從節點 node 開始搜索 key
+    for (size_t i = 0; i < key.length; i++)
+    {
+      if (p == nullptr)
+      {
+        // reach the end of the node
+        return nullptr;
+      }
+
+      // There is still nodes to traverse
+      char c = key[i];
+      // p = p->children[c]
+      // do i need unsigned char?
+      p = p->children[static_cast<unsigned char>(c)];
+    }
+
+    return p;
   }
 };
