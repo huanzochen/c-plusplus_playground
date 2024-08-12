084c568fa300cf8959728d5b71ba9135a1b39e66
tzuchuan.huang <cy94295@gmail.com>
2024-08-12 19:32:59 +0800

    feat: double linked list

--- algo/structures/doubleLinkedList.cpp
+++ algo/structures/doubleLinkedList.cpp
@@ -1,71 +1,104 @@
+#include <iostream>
+
+using namespace std;
 class Node
 {
 public:
   int key;
   int val;
 
-  Node *next; // pointer to the next node.
-  Node *prev; // pointer to the previous node.
+  Node *next;
+  Node *prev;
 
-  // Constructor
-  Node(int k, int v) : key{k}, val{v}, next{nullptr}, prev{nullptr}
+  Node(int key, int value) : key{key}, val{value}
+  {
+    next = nullptr;
+    prev = nullptr;
+  }
+
+  ~Node()
   {
   }
 };
 
 class DoubleLinkedList
 {
-public:
   Node *head;
   Node *tail;
-  int size;
 
-  // Constructor
-  DoubleLinkedList() : size{0}
+public:
+  DoubleLinkedList()
   {
     head = new Node(0, 0);
     tail = new Node(0, 0);
 
-    (*head).next = tail;
-    (*tail).prev = head;
+    // make connection
+    head->next = tail;
+    tail->prev = head;
   }
 
   ~DoubleLinkedList()
   {
-    delete head;
-    delete tail;
+    // Delete all nodes between head and tail
+    Node *current = head;
+    while (current != nullptr)
+    {
+      Node *nextNode = current->next;
+      delete current;
+      current = nextNode;
+    }
   }
 
-  // 在尾部添加新節點 x, 時間 O(1)
-  void addLast(Node *x)
+  void addLast(int key, int val)
   {
-    x->prev = tail->prev;
-    x->next = tail;
-    tail->prev->next = x;
+    Node *newNode = new Node(key, val);
 
-    // 最後再把 tail->prev 洗成 x
-    tail->prev = x;
-    size++;
+    newNode->prev = tail->prev;
+    newNode->next = tail;
+    tail->prev->next = newNode;
+    // 最後再洗掉，要對 tail->prev 做一些事
+    tail->prev = newNode;
   }
 
   // 刪除鏈表中的 x 節點 (x 一定存在)
   // 由於是雙鏈表而且給的是特定 Node 節點, 時間 O(1)
   void remove(Node *x)
   {
-    x->prev->next = x->next;
+    // 把 x 抽掉
     x->next->prev = x->prev;
-    size--;
+    x->prev->next = x->next;
   }
 
   // 刪除鏈表中的第一個節點，並返回該節點，時間 O(1)
   Node *removeFirst()
   {
     if (head->next == tail)
+    {
       return nullptr;
+    }
     Node *first = head->next;
     remove(first);
     return first;
   }
 
-  int size() { return size; }
+  void printAllNode()
+  {
+    Node *p1 = this->head;
+    cout << "=======printAllNode" << endl;
+    while (p1 != nullptr)
+    {
+      cout << "p1->key: " << p1->key << " ,val: " << p1->val << " ";
+      p1 = p1->next;
+    }
+    cout << "================" << endl;
+  }
 };
+
+int main()
+{
+  DoubleLinkedList du;
+
+  du.addLast(2, 12);
+  du.addLast(3, 34);
+  du.printAllNode();
+}
