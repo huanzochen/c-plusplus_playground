6fe5253d3178e040ad64c61fa2eb7531473c6420
tzuchuan.huang <cy94295@gmail.com>
2024-08-23 21:55:32 +0800

    feat: string comparison

---
+++ tips/string_comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+  string curr = "59";
+  string target = "60";
+
+  cout << "(curr < target):" << (curr < target) << endl;
+  cout << "(curr > target):" << (curr > target) << endl;
+}
