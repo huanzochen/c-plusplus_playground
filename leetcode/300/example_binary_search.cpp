df30b0a89b0961e34ff8472f3ffdc2297ec93eff
Huang, TzuChuan | Tommy | TWR <tzuchuan.huang@rakuten.com>
2024-08-29 15:44:28 +0800

    feat: binary search

---
+++ leetcode/300/example_binary_search.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+      vector<int> top(nums.size(), 0);
+
+      int piles = 0;
+
+      for(int i = 0; i < nums.size(); i++){
+        // 要處理的撲克牌
+        int poker  = nums[i];
+
+        // 二分查找
+        int left = 0, right = piles;
+        while(left < right){
+          int mid = (left + right) / 2;
+          if(top[mid] > poker){
+            right = mid;
+          } else if(top[mid] < poker){
+            left = mid + 1;
+          } else {
+            right = mid;
+          }
+        }
+
+        // 沒找到合適的牌堆，新建一堆
+        if(left == piles) piles++;
+
+        // 把這張牌堆放到堆頂
+        top[left] = poker;
+      }
+
+      return piles;
+    }
+};
+
+int main()
+{
+
+    vector<int> nums{10, 9, 2, 5, 3, 7, 101, 18};
+
+    Solution *sol;
+
+    cout << (sol->lengthOfLIS(nums) == 4) << endl;
+}
