f20131f6bb941490db567564143583bd9bd086fa
tzuchuan.huang <cy94295@gmail.com>
2024-10-02 19:29:04 +0800

    feat: for loop test

--- basics/forloop.cpp
+++ basics/forloop.cpp
@@ -23,14 +23,32 @@ int main()
 
   int a = 0;
   cout << "a++: " << a++ << endl;
-  cout << "With ++a, it will display after increment" << endl << "++a: " << ++a << endl;
+  cout << "With ++a, it will display after increment" << endl
+       << "++a: " << ++a << endl;
 
-  for(auto name: names){
+  for (auto name : names)
+  {
     cout << "name: " << name << endl;
   }
   cout << endl;
-  for(auto& current: names) {
+  for (auto &current : names)
+  {
     cout << "name: " << current << endl;
   }
 
+  // run a code that will not run
+
+  vector<int> nums(0);
+
+  for (int i = 0; i < nums.size(); i++)
+  {
+    cout << "It will not run?" << endl;
+  }
+
+  vector<int> nums2{1, 2};
+
+  for (int i = 2; i < nums2.size(); i++)
+  {
+    cout << "It will not run?" << endl;
+  }
 }
